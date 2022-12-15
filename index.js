0e27dc4b8d6912e58680726b7f7b9aa4b630da9b
Colton Brenneman <colton.brenneman@gmail.com>
2022-12-14 22:39:11 -0700

    commit tree 2e80c5954cec6284531ef5aebfa09801c5c74159

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
 
 // Write your code here
+
+let num1 = 2
+let num2 = 31
+let multiply = num1 * num2
+
+let random = Math.floor(Math.random() * 9) +1;
+
+let num3 = 4
+let num4 = 32
+
+let rem = num3 % num4;
+
+let max = Math.max(5, 10, 15, 20);
